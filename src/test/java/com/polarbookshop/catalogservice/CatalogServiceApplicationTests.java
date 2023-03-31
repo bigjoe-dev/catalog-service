4bd004cce23ab2a945fc911d9bca42966efb9473
bigjoe-dev <hello@joedelamore.com>
2023-03-31 21:21:06 +0200

    feat: add publisher field

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -14,7 +14,7 @@ class CatalogServiceApplicationTests {
 	private WebTestClient webTestClient;
 	@Test
 	void whenPostRequestThenBookCreated() {
-		var expectedBook = Book.of("1231231231", "Title", "Author", 9.90);
+		var expectedBook = Book.of("1231231231", "Title", "Author", 9.90, "Polarsophia");
 
 		webTestClient
 				.post()
